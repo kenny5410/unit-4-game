6117ee8ccdb65d8eac65debeca3178cc748a9228
Kenny <nels5410@gmail.com>
2019-01-06 17:58:50 -0600

    game almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,104 @@
 
-$(document).ready(function() {
     //Runs on page load
-    /*window.onload = function() {
+    window.onload = function() {
         $('#crystal-one').on('click', crystal_one);
         $('#crystal-two').on('click', crystal_two);
         $('#crystal-three').on('click', crystal_three);
         $('#crystal-four').on('click', crystal_four);
-    }*/
+    }
 
 
     //Defining global varibles
     var wins = 0;
     var loses = 0;
     var randomNumber;
-    var total;
+    var totalValue;
     var crystal_one;
     var crystal_two;
     var crystal_three;
-    var cryastal_four;
+    var crystal_four;
 
     //Starting game function
     function initializeGame() {
         wins = "";
         loses = "";
-        total = 0;
+        totalValue = 0;
         randomNumber = Math.floor(Math.random() * 120) + 19;
         crystal_one = Math.floor(Math.random() * 12) + 1;
         crystal_two = Math.floor(Math.random() * 12) + 1;
         crystal_three = Math.floor(Math.random() * 12) + 1;
         crystal_four = Math.floor(Math.random() * 12 + 1);
         $('#random-number').html(randomNumber);
-        $('#total').html(total);
+        $('#total').html(totalValue);
     }
 
     initializeGame();
 
     //Reset Game function
     function resetGame() {
-        total = 0;
+        totalValue = 0;
         randomNumber = Math.floor(Math.random() * 101) + 19;
         crystal_one = Math.floor(Math.random() * 12) + 1;
         crystal_two = Math.floor(Math.random() * 12) + 1;
         crystal_three = Math.floor(Math.random() * 12) + 1;
         crystal_four = Math.floor(Math.random() * 12 + 1);
         $('#random-number').html(randomNumber);
-        $('#total').html(total);
+        $('#total').html(totalValue);
     };
 
     //Win and Loss conditions
     function winner() {
         wins++;
-        $('wins').html('Wins: '+ wins);
+        $('#wins').html('Wins: ' + wins);
+        resetGame();
     };
 
     function loser() {
         loses++
-        $('losses').html('Loses: '+ loses);
+        $('#loses').text('Loses: ' + loses);
         resetGame();
     };
 
     //Stuff is happening here
-    $('#crystal-one, #crystal-two, #crystal-three, #crystal-four').click(function() {
-        $('')
-        if ( total === randomNumber) {
+    $('#crystal-one').on('click', function() {
+        totalValue += crystal_one;
+        $('#total').html(totalValue);
+        if ( totalValue === randomNumber) {
             winner();
         }
-        if ( total > randomNumber) {
+        if ( totalValue > randomNumber) {
+            loser();
+        }
+    });
+    $('#crystal-two').on('click', function() {
+        totalValue += crystal_two;
+        $('#total').html(totalValue);
+        if ( totalValue === randomNumber) {
+            winner();
+        }
+        if ( totalValue > randomNumber) {
+            loser();
+        }
+    });
+    $('#crystal-three').on('click', function() {
+        totalValue += crystal_three;
+        $('#total').html(totalValue);
+        if ( totalValue === randomNumber) {
+            winner();
+        }
+        if ( totalValue > randomNumber) {
+            loser();
+        }
+    });
+    $('#crystal-four').on('click', function() {
+        totalValue += crystal_four;
+        $('#total').html(totalValue);
+        if ( totalValue === randomNumber) {
+            winner();
+        }
+        if ( totalValue > randomNumber) {
             loser();
         }
     });
 
-   
-});
+
